ba33174fbbcd115ce32bf93048fcb22b9bfee6b7
chuxuewen <louistru@hotmail.com>
2022-03-17 16:04:12 +0800

    update test

--- solidity/Test.sol
+++ solidity/Test.sol
@@ -1,64 +1,65 @@
 pragma solidity ^0.4.24;
 
 contract Context {
-	address __impl;
-	uint256[50] __gap;
-	
-	// function delegateCall(bytes data) public {
-	//     this.call.value(msg.value)(data);
-	// }
+		address __impl;
 
-	// function getPositionAt(uint n) public view returns (address) {
-	//     assembly {
-	//         let d := sload(n)
-	//         mstore(0x80, d)
-	//         return(0x80,32)
-	//   }
-	// }
+		// function delegateCall(bytes data) public {
+		//     this.call.value(msg.value)(data);
+		// }
 
-	constructor(address impl) {
-		require(impl != address(0));
-		__impl = impl;
-	}
+		// function getPositionAt(uint n) public view returns (address) {
+		//     assembly {
+		//         let d := sload(n)
+		//         mstore(0x80, d)
+		//         return(0x80,32)
+		//   }
+		// }
 
-	function() payable public {
-		address _impl = __impl;
-		require(_impl != address(0));
-
-		assembly {
-			let ptr := mload(0x40) 
-			calldatacopy(ptr, 0, calldatasize) 
-			let result := delegatecall(gas, _impl, ptr, calldatasize, 0, 0)
-			let size := returndatasize 
-			returndatacopy(ptr, 0, size) 
-
-			switch result
-			case 0 { revert(ptr, size) }
-			default { return(ptr, size) }
+		constructor(address impl) {
+				require(impl != address(0));
+				__impl = impl;
 		}
-	}
 
+		function() payable public {
+				address _impl = __impl;
+				require(_impl != address(0));
+
+				assembly {
+						let ptr := mload(0x40) 
+						calldatacopy(ptr, 0, calldatasize) 
+						let result := delegatecall(gas, _impl, ptr, calldatasize, 0, 0)
+						let size := returndatasize 
+						returndatacopy(ptr, 0, size) 
+
+						switch result
+						case 0 { revert(ptr, size) }
+						default { return(ptr, size) }
+				}
+		}
+
+	uint256[50] __gap;
+		
 }
 
 contract Logic {
-	address public impl;
-	uint256 public a;
-	uint256 public b;
-	uint256 public c;
+		address public impl;
+		uint256 public a;
+		uint256 public b;
+		uint256 public c;
 
-	function upgrade(address _impl) public {
-		impl = _impl;
-	}
+		function upgrade(address _impl) public {
+				impl = _impl;
+		}
 
-	function A(uint256 v) public {
-		a = v;
-	}
+		function A(uint256 v) public {
+				a = v;
+		}
 
-	function B(uint256 v) public {
-		b = v;
-	}
+		function B(uint256 v) public {
+				b = v;
+		}
 
-	function C(uint256 v) public {
-		c = v;
-	}
+		function C(uint256 v) public {
+				c = v;
+		}
 }
